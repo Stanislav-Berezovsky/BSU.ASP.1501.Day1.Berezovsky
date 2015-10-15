30be3aa4f16adcedef81d4c33e8a3840a0b980c7
stas <stanislav.berezovsky.95@gmail.com>
2015-10-15 12:55:06 +0300

    TheFirst

---
+++ Task1/FindingTheRoot.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    public static class FindingTheRoot
+    {
+        public static double NewtonMethod(double number, int exponent, double correctness)
+        {
+            if (number < 0 && exponent % 2 == 0)
+            {
+                return double.NaN;
+            }
+            else
+                if (exponent == 0)
+                {
+                    return 1;
+                }
+                else
+                {
+                    double x = number;
+                    double xk = 0;
+                    while (correctness < Math.Abs(x - xk))
+                    {
+                        xk = x;
+                        x = 1.0 / exponent * ((exponent - 1) * xk + number / Math.Pow(xk, exponent - 1));
+                    }
+                    return x;
+                }
+        }
+    }
+}
